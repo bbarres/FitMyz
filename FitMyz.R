c652e2ed57603f940cb6fb2aa61f9ae38bef0821
bbarres <benoit.barres@gmail.com>
2016-08-24 13:53:49 +0200

    update "age" model and adding a first draft of the "nb_larve" model

--- FitMyz.R
+++ FitMyz.R
@@ -23,30 +23,58 @@ single<-read.table("singledata.txt",header=TRUE,sep='\t')
 
 #let see the structure of the data table
 summary(single)
+#we remove samples with no laid larvae or with clone dying during the first day
+single<-single[single$age>1 & single$nb_larve>0,]
+
+##life expectancy of the clone####
 #and the distribution of the variable of interest
-hist(single$age,by=single$Clone)
+op<-par(mfrow=c(3,1))
+plot(density(single$age[single$Clone=="13 001 041"]))
+hist(single$age[single$Clone=="13 001 048"])
+hist(single$age[single$Clone=="13 001 050"])
+par(op)
+
 
 #we first build a complete model with all interaction included for the life 
 #expectancy of the clone
 #the explainatory variables are the Clone ID, the Posca treatment and whether 
 #or not the larvae were removed along the experiment
-modage<-glm(age~Clone*Posca*Larves_enlevees*date,family="poisson",data=single)
+modage<-glm(age~Clone*Posca*Larves_enlevees,family="poisson",data=single)
 summary(modage)
 
-#because the interactions do not show signigicant effect on the response 
-#variable, we remove the interactions from the model
-modage<-glm(age~Clone+Posca+Larves_enlevees+date,family="poisson",data=single)
+#we iteratively simplify the model by removing non-significant interactions
+#and factors
+modage<-glm(age~Clone+Posca+Larves_enlevees,family="poisson",data=single)
 summary(modage)
-plot(modage)
-modage<-glm(age~Clone+Posca+date,family="poisson",data=single)
-summary(modage)
-plot(modage)
-modage<-glm(age~Clone+Posca,family="poisson",data=single)
-summary(modage)
-drop1(modage)
-plot(modage)
+#here is the final model
+modagef<-glm(age~Clone+Posca,family="poisson",data=single)
+summary(modagef)
+plot(modagef)
 
+##number of larvae produce
+#we remove the repetition where the larvae weren't remove along the 
+#experiment since they may have contributed to the total number of larvae
+single2<-single[single$Larves_enlevees!="Non",]
+#we first build a complete model with all interaction included for the 
+#production of larvae
+#the explainatory variables are the Clone ID, the Posca treatment and whether 
+#or not the larvae were removed along the experiment
+modnbl<-glm(nb_larve~Clone*Posca,family="poisson",data=single2)
+summary(modnbl)
 
+#we iteratively simplify the model by removing non-significant interactions
+#and factors
+modnbl<-glm(nb_larve~Clone+Posca+Larves_enlevees+Clone:Larves_enlevees+Clone:Posca,
+            family="poisson",data=single)
+summary(modnbl)
+modnbl<-glm(nb_larve~Clone+Posca,family="poisson",data=single)
+summary(modnbl)
+modnbl<-glm(nb_larve~Clone+Posca+Larves_enlevees,family="poisson",data=single)
+summary(modnbl)
+#here is the final model
+modnblf<-glm(nb_larve~Clone+Posca,family="poisson",data=single)
+summary(modnblf)
+plot(modnblf)
 
 
 
